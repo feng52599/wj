2bb80437b0a9ccaf74bff6b8f115c7338d7f95ea
feng52599 <1143874188@qq.com>
2020-05-18 18:35:01 +0800

    ①解决新版本json数据生成错误导致children无数据问题---可以运行

--- src/main/java/edu/feng/wj/controller/MenuController.java
+++ src/main/java/edu/feng/wj/controller/MenuController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -23,6 +24,17 @@ public class MenuController {
     @GetMapping("/api/menu")
     public List<AdminMenu> menu(){
         List <AdminMenu> menus =  adminMenuService.getMenusByCurrentUser();
+        for (AdminMenu menu : menus) {
+            menu.setChildren(adminMenuService.getAllByParentId(menu.getId()));
+        }
+
+        Iterator<AdminMenu> iterator = menus.iterator();
+        while (iterator.hasNext()) {
+            AdminMenu menu = iterator.next();
+            if (menu.getParentId() != 0) {
+                iterator.remove();
+            }
+        }
         return menus;
     }
 }
