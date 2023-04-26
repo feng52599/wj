1867bd1b64d6ce7a099b9389e4f443e632b67ca6
feng52599 <1143874188@qq.com>
2023-04-26 22:14:57 +0800

    增加redis功能

--- src/main/java/edu/feng/wj/controller/LibraryController.java
+++ src/main/java/edu/feng/wj/controller/LibraryController.java
@@ -1,21 +1,23 @@
 package edu.feng.wj.controller;
 
 import edu.feng.wj.pojo.Book;
+import edu.feng.wj.resp.BookResp;
 import edu.feng.wj.result.Result;
 import edu.feng.wj.result.ResultFactory;
 import edu.feng.wj.service.BookService;
 import edu.feng.wj.service.RedisService;
 import edu.feng.wj.utils.CastUtils;
 import edu.feng.wj.utils.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @program: wj
@@ -34,19 +36,69 @@ public class LibraryController {
     // 在MyWebConfigurer.java设置允许所有请求跨域，便可以不用@CrossOrigin
     @GetMapping("/api/books")
 //    @CrossOrigin
-    public List<Book> list() {
+    public List<BookResp> list() {
         List<Book> books;
+        // List <Book> returnList = new ArrayList<>();
         String key = "booklist";
+        // Object bookCache = null ;
         Object bookCache = redisService.get(key);
 
         if (bookCache == null) {
             // Sort sort = new Sort(Sort.Direction.DESC, "id");
             books = bookService.list();
+            // 这边的操作不对
             redisService.set(key, books);
         } else {
             books = CastUtils.objectConvertToList(bookCache, Book.class);
         }
-        return books;
+
+        String s = "";
+        // String a = null;
+
+        // 取出这个books之后要做一些处理要
+        // 1.过滤
+        // List<Book> returnList = books.stream().filter(v -> v.getAuthor().equals("余秀华")).collect(Collectors.toList());
+        // 2.自然排序
+        // List<Book> returnList = books.stream().sorted().collect(Collectors.toList());
+
+        // 3.非自然排序
+        // List<Book> returnList = books.stream().sorted(Comparator.comparing(Book::getAuthor)).collect(Collectors.toList());
+        // List<Book> returnList = books.stream().sorted((o1, o2) -> {
+        //     if (o1.getId() == o2.getId()) {
+        //         return 0;
+        //     } else {
+        //         return o1.getId() > o2.getId() ? -1 : 1;
+        //     }
+        // }).collect(Collectors.toList());
+
+        // 4.map
+        // List<String> collect = books.stream().map(Book::getAuthor).collect(Collectors.toList());
+        // collect.forEach(System.out::println);
+
+        // limit
+        // List<Book> returnList = books.stream().filter(v -> "余秀华".equals(v.getAuthor())).limit(1).collect(Collectors.toList());
+
+        // distinct 输出所有作者名字
+        List<String> Authors = books.stream().map(Book::getAuthor).distinct().collect(Collectors.toList());
+        Authors.forEach(System.out::println);
+
+        // count
+        long count = books.stream().map(Book::getAuthor).distinct().count();
+        System.out.println("Author Num : " + count);
+        // group BY
+        Map<String, List<Book>> map = books.stream().collect(Collectors.groupingBy(Book::getAuthor));
+
+        // 有键值冲突，选前者，参考hashMap 此处JDK8 有bug
+        Map<String, String> authorToTitle = books.stream().filter(v -> null != v).collect(Collectors.toMap(Book::getAuthor, Book::getTitle, (a, b) -> a));
+        books.stream().map(Book::getAuthor).collect(Collectors.toList());
+        // map 函数式编程
+        List<BookResp> returnList = books.stream().map(book -> {
+            BookResp bookResp = new BookResp();
+            BeanUtils.copyProperties(book, bookResp);
+            return bookResp;
+        }).collect(Collectors.toList());
+
+        return returnList;
     }
 
 
@@ -117,7 +169,6 @@ public class LibraryController {
             return "";
         }
     }
-
 
 
 }
---
+++ src/main/java/edu/feng/wj/resp/BookResp.java
@@ -0,0 +1,17 @@
+package edu.feng.wj.resp;
+
+import lombok.Data;
+
+/**
+ * @program: wj
+ * @description: 
+ * @author: feng
+ * @create: 2021-11-24 22:00
+ * @version: 1.0
+ */
+@Data
+public class BookResp {
+    private String date;
+    private String press;
+    private String abs;
+}
